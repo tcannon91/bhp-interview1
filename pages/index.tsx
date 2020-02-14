7ba3ee12a83354643fd67417005685df4c69b309
Taylor Cannon <tcannon@brighthealthplan.com>
2020-02-14 08:31:52 -0600

    Same Game Starter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,18 @@
-import { Container, Jumbotron } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
-function HomePage() {
+function SameGame() {
   return (
     <Container>
-      <Jumbotron>
-        <h1>Instructions</h1>
-        <p>We will be creating a new page that will list all of Facebook's github repositories.</p>
-        <ul>
-            <li>Path to page should be /repos</li>
-            <li>API endpoint: <a href='https://api.github.com/orgs/facebook/repos'>https://api.github.com/orgs/facebook/repos</a></li>
-            <li>Ability to navigate there from this page</li>
-        </ul>
-        <h2>Extra Notes</h2>
-        <ul>
-            <li>This is a Next.js application</li>
-            <li>This app uses <a href='https://reactstrap.github.io/components/alerts/'>reactstrap</a> for some basic styling</li>
-            <li>You are free to pull in any libraries you would like</li>
-        </ul>
-      </Jumbotron>
+      { /* This is just a sample row, feel free to run with this visual system, or leverage your own */ }
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
     </Container>
   );
 }
 
-export default HomePage;
+export default SameGame;
