1477361d896a07109163000cac6139ecf507494b
Taylor Cannon <tcannon@brighthealthplan.com>
2020-04-21 08:16:32 -0500

    More clear starting point for candidates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row } from 'reactstrap';
+
+const createBox = (color: string) => (
+  <div style={{
+    height: '50px',
+    width: '50px',
+    backgroundColor: color,
+    border: 'white 1px solid',
+  }}>
+  </div>
+);
 
 function SameGame() {
   return (
     <Container>
       { /* This is just a sample row, feel free to run with this visual system, or leverage your own */ }
       <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
+        <div>{createBox('red')}</div>
+        <div>{createBox('green')}</div>
+        <div>{createBox('blue')}</div>
+        <div>{createBox('yellow')}</div>
+        <div>{createBox('black')}</div>
       </Row>
     </Container>
   );
